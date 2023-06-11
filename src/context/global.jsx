c9d083b58104d4d5f95d8f4dce448be2123b8894
isaba nusrat <isabansrt@gmail.com>
2023-06-10 23:43:41 -0700

    add api link, set initial state

--- src/context/global.jsx
+++ src/context/global.jsx
@@ -2,9 +2,35 @@ import React, { createContext, useContext } from "react";
 
 const GlobalContext = createContext();
 
+const baseUrl = "https://api.jikan.moe/v4";
+
+//reducer
+const reducer = (state, action) => {
+  return state;
+};
+
 export const GlobalContextProvider = ({ children }) => {
+  //initial state
+  const initialState = {
+    popularAnime: [],
+    upcomingAnime: [],
+    airingAnime: [],
+    pictures: [],
+    isSearch: [],
+    searchResults: [],
+    loading: false,
+  };
+
+  const [state, dispatch] = useReducer(reducer, initialState);
+
   return (
-    <GlobalContext.Provider value={"hello"}>{children}</GlobalContext.Provider>
+    <GlobalContext.Provider
+      value={{
+        ...state,
+      }}
+    >
+      {children}
+    </GlobalContext.Provider>
   );
 };
 
