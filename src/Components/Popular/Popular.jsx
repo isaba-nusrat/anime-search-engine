aaf6dd5c56d9b8ea37a5c67c629426ffdbdad083
isaba nusrat <isabansrt@gmail.com>
2023-06-11 09:58:29 -0700

    fetch image from api and display

--- src/Components/Popular/Popular.jsx
+++ src/Components/Popular/Popular.jsx
@@ -1,9 +1,26 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { useGlobalContext } from "../../context/global";
+import "./Popular.scss";
 
 function Popular() {
+  const { popularAnime, isSearch } = useGlobalContext();
+
+  const conditionalRender = () => {
+    if (!isSearch) {
+      return popularAnime.map((anime) => {
+        return (
+          <Link to={`/anime/${anime.mal_id}`} key={anime.mal_id}>
+            <img src={anime.images.jpg.large_image_url} alt="" />
+          </Link>
+        );
+      });
+    }
+  };
+
   return (
-    <div>
-      <h1>popular</h1>
+    <div className="popular-anime">
+      <div className="popular-anime__display-box">{conditionalRender()}</div>
     </div>
   );
 }
